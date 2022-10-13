5bc0985908308898d600c7a7da2699cff4fd356c
Evan B <evanpbishop@gmail.com>
2022-10-13 16:38:14 -0400

    add waybar

--- homes.nix
+++ homes.nix
@@ -4,12 +4,14 @@
   ];
   home-manager.users.epb = {
     services = {
+      mpd.enable = true;
       gammastep = {
         enable = true;
         longitude = -80.0;
         latitude = 40.0;
       };
     };
+    programs.waybar.enable = true;
     home = { packages = with pkgs;[ ]; stateVersion = "22.05"; };
   };
 }
