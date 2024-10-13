814d3345a092859d9f94433ae4b7c8bd0d8ffd98
hyperboid <hyperboid@nullanoid.net>
2024-10-13 09:08:26 -0400

    Use nixpkgs 24.05 instead of unstable for base Orig Rebuild: 88 current  2024-10-13 09:08:25  24.05.20240531.63dacb4  6.6.32                          *

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
