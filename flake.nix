a7cc46dd40cccf44e415c2867c96da3cda02358f
hyperboid <hyperboid@nullanoid.net>
2024-10-13 05:51:09 -0400

    The Alleged Modularization

--- flake.nix
+++ flake.nix
@@ -19,13 +19,13 @@
       hyperboid-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
-          ./gpucompute.nix
+          ./laptop/configuration.nix
+          ./laptop/gpucompute.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.hyperboid = import ./homes/hyperboid.nix;
+            home-manager.users.hyperboid = import ./laptop/homes/hyperboid.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
